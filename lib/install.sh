fe900613160519b2ee5e0e2a266c97c500afb4b7
Serhat Teker <serhat.teker@gmail.com>
2022-05-20 22:32:01 +0300

    Fix INVENV python script in install.sh script

--- lib/install.sh
+++ lib/install.sh
@@ -6,12 +6,20 @@ PYTHON_VERSION="${PYTHON_VERSION_USE:-3.8}"
 PYTHON_BIN=$(which python${PYTHON_VERSION})
 
 # Are you in virtualenv
-INVENV=$(${PYTHON_BIN} -c 'import sys; print ("1" if hasattr(sys, "real_prefix") else "0")')
+INVENV=$(${PYTHON_BIN} -c 'import sys
 
 
-if [ ! ${INVENV} ]; then
-  echo "You already in virtualenv. deactivate first."
-  exit 1
+def is_venv():
+    return hasattr(sys, "real_prefix") or (
+        hasattr(sys, "base_prefix") and sys.base_prefix != sys.prefix
+    )
+
+print(is_venv())
+')
+
+if [ ${INVENV} == "True" ]; then
+    echo "You already in virtualenv, deactivate first."
+    exit 1
 fi
 
 PIP_REQUIRE_VIRTUALENV=false ${PYTHON_BIN} -m pip install --user doq
