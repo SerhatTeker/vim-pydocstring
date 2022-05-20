95b0fd1be6cf438b89f202b50e23686e83b55ab8
Serhat Teker <serhat.teker@gmail.com>
2022-05-20 22:11:19 +0300

    Fix INVENV python interpreter in install.sh script

--- lib/install.sh
+++ lib/install.sh
@@ -2,18 +2,18 @@
 
 set -eu
 
+PYTHON_VERSION="${PYTHON_VERSION_USE:-3.8}"
+PYTHON_BIN=$(which python${PYTHON_VERSION})
+
 # Are you in virtualenv
-INVENV=$(python -c 'import sys; print ("1" if hasattr(sys, "real_prefix") else "0")')
+INVENV=$(${PYTHON_BIN} -c 'import sys; print ("1" if hasattr(sys, "real_prefix") else "0")')
+
 
 if [ ! ${INVENV} ]; then
   echo "You already in virtualenv. deactivate first."
   exit 1
 fi
 
-PYTHON_VERSION="${PYTHON_VERSION_USE:-3.8}"
-PYTHON_BIN=$(which python${PYTHON_VERSION})
-PYTHON=${PYTHON_BIN}
-
-${PYTHON} -m pip install --user doq
+PIP_REQUIRE_VIRTUALENV=false ${PYTHON_BIN} -m pip install --user doq
 
 exit 0
